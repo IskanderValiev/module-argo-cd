b6258b6a7a893512144d513428f18933cd7af08b
Iskander Valiev <iskander.valiev@ee.co.uk>
2025-12-08 15:57:29 +0300

    fixed helm provider

--- main.tf
+++ main.tf
@@ -9,15 +9,6 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
-    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
-    host                   = var.kubernetes_cluster_endpoint
-    exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
-      command     = "aws-iam-authenticator"
-      args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
-    }
-  }
 }
 
 resource "kubernetes_namespace_v1" "example" {
